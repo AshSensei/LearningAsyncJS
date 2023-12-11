3e254eeddf6aa7357271db206bd59e99dc3d718c
AshSensei <an9simbol@gmail.com>
2023-12-10 20:22:46 -0800

    did implement promise api practice

--- practice.js
+++ practice.js
@@ -1,6 +1,6 @@
 //synchronous function
-const name = "Miriam";
-const greeting = `Hello, my name is ${name}!`;
+const name1 = "Miriam";
+const greeting = `Hello, my name is ${name1}!`;
 console.log(greeting);
 // "Hello, my name is Miriam!"
 
@@ -19,3 +19,37 @@ console.log(greeting);
 
 //async keyword makes a function an async function and you can make sure a call awaits a process and it will return a complete response object
 //only use await inside an async function
+
+const label = document.querySelector("#name");
+const delay = document.querySelector("#delay");
+const button = document.querySelector("#set-alarm");
+const output = document.querySelector("#output");
++
+
+function setAlarm() {
+  setTimeout(() => {
+    output.textContent = "Wake up!";
+  }, 1000);
+};
+
+button.addEventListener("click", alarm);
+
+function alarm(person, delay) {
+  return new Promise((resolve, reject) => {
+    if (delay < 0) {
+      throw new Error("Alarm delay must not be negative");
+    }
+    setTimeout(() => {
+      resolve(`Wake up, ${person}!`);
+    }, delay);
+  });
+}
+
+button.addEventListener("click", async () => {
+  try {
+    const message = await alarm(label.value, delay.value);
+    output.textContent = message;
+  } catch (error) {
+    output.textContent = `Couldn't set alarm: ${error}`;
+  }
+});
